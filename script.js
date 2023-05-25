a6a78349841b53d90010de846e3a4823c273e863
HigorSouza <higor.andrade@hotmail.com>
2023-05-24 21:14:42 -0300

    separação de paginas e inclusão do arquivo js

---
+++ script.js
@@ -0,0 +1,17 @@
+function direcionaempresario() {
+  window.location.href = "empresario/main-screen-empresario.html";
+}
+
+function direcionacliente() {
+  window.location.href = "cliente/main-screen-cliente.html";
+}
+
+function direcionaLoginGoogle() {
+  window.location.href = "login/login-google.html";
+}
+
+function direcionaLoginApple() {
+  window.location.href = "login/login-apple.html";
+}
+
+
