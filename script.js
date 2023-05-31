0d1844f0296995c9e17f1539390912b5778ce721
HigorSouza <higor.andrade@hotmail.com>
2023-05-31 20:55:15 -0300

    Ajustes feitos em sala

--- script.js
+++ script.js
@@ -28,3 +28,20 @@ function direcionaajuda() {
 function direcionaCriarConta() {
   window.location.href = "criar-conta.html";
 }
+
+function direcionaTelaEmpresario() {
+  window.location.href = "criar-contaEmpresario.html";
+}
+
+function direcionaTelaCliente() {
+  window.location.href = "criar-conta.html";
+}
+
+function direcionaempresario() {
+  window.location.href = "empresario/main-screen-empresario.html";
+}
+
+function direcionaempresario2() {
+  window.location.href = "contaEmpresario2.html"
+}
+
